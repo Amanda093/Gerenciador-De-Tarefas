ad8bc88c4e1d54c95af21194cd947dacaabe3a4a
Amanda093 <vovigadagamer@gmail.com>
2024-12-29 16:08:10 -0300

    usando state

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 // Componente
 function App() {
-  // State (Estado)
-  return(
+  const [message, setMessage] = useState("Olá mundo!");
+
+  return (
+    <div>
+      <h1>{message}</h1>
+      <button
+        onClick={() => {
+          setMessage("Olá, fui clicado!");
+        }}
+      >
+        Mudar mensagem
+      </button>
+    </div>
   );
 }
 
