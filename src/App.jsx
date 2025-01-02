953b322949fd00b45da2da594f2beeb43ae65a9f
Amanda093 <vovigadagamer@gmail.com>
2025-01-02 13:21:23 -0300

    fiz a parte do task

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,50 @@
 import { useState } from "react";
+import AddTask from "./components/AddTask";
+import Tasks from "./components/Tasks";
+import Title from "./components/Title";
+import "./index.css"; // Ajuste o caminho se necessário
 
-// Componente
 function App() {
-  const [message, setMessage] = useState("Olá mundo!");
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar",
+      description: "Estudar 8 horas por dia",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Estudar matematica",
+      description: "Estudar 8 horas por dia",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Estudar ingles",
+      description: "Estudar 8 horas por dia",
+      isCompleted: false,
+    },
+  ]);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      // Preciso atualizar a tarefa
+      if (task.id === taskId) {
+        return { task, isCompleted: !task.isCompleted };
+      }
+
+      // Não preciso atualizar a tarefa
+      return newTasks;
+    });
+  }
 
   return (
-    <div>
-      <h1>{message}</h1>
-      <button
-        onClick={() => {
-          setMessage("Olá, fui clicado!");
-        }}
-      >
-        Mudar mensagem
-      </button>
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <Title>Gerenciador de Tarefas</Title>
+        <AddTask />
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} />
+      </div>
     </div>
   );
 }
